48e14c86a9d2fd3200e1d958100938f84bc2f2f6
Marcel van Vliet <marcel.vanvliet@trradifyhq.com>
2021-10-26 19:42:16 +1300

    Add keybindings json settings

--- scripts/10-configure-vs-code.sh
+++ scripts/10-configure-vs-code.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env zsh
-echo "Configuring VS Code..."
+echo "Configuring VS Code settings..."
 rm ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/.setup/dotfiles/vscode.settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+echo "Configuring VS Code keybindings..."
+rm ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/.setup/dotfiles/vscode.keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
