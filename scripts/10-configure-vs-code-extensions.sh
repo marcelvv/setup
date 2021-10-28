42733b29ebd61790f85100585022b876c16041aa
marcelvv <92703198+marcelvv@users.noreply.github.com>
2021-10-28 17:45:31 +1300

    disable dbt-poweruser

--- scripts/10-configure-vs-code-extensions.sh
+++ scripts/10-configure-vs-code-extensions.sh
@@ -11,7 +11,7 @@ code --install-extension ms-vsliveshare.vsliveshare --force
 code --install-extension ms-vscode-remote.remote-containers --force
 code --install-extension bastienboutonnet.vscode-dbt --force
 code --install-extension coenraads.bracket-pair-colorizer --force
-code --install-extension innoverio.vscode-dbt-power-user --force
+# code --install-extension innoverio.vscode-dbt-power-user --force
 code --install-extension ms-azuretools.vscode-docker --force
 code --install-extension ms-python.python --force
 code --install-extension itsvipa.vscode-sqlfluff-unofficial --force
